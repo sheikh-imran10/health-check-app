c239d3dfe477281ac17ecedbb212eb639438cff7
sheikh-imran10 <imran.sheikh@bertelsmann.de>
2026-05-18 11:39:12 +0800

    first commit

---
+++ app/config.py
@@ -0,0 +1,26 @@
+import os
+
+
+class BaseConfig:
+    SECRET_KEY = os.getenv("SECRET_KEY", "change-me-in-production")
+    JSON_SORT_KEYS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+
+
+config_map = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+}
